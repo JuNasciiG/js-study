062d42237c63250eff22ae68e46e5640e1b6a790
JuNascii <65746158+JuNasciiG@users.noreply.github.com>
2026-02-07 11:31:31 -0300

    Simple JavaScript console.log commands.

---
+++ 01-basics/strings/js/main.js
@@ -0,0 +1,4 @@
+console.log('hello world');
+console.log('JuNascii is a' );console.log(typeof'JuNascii');
+console.log('42 is a');console.log(typeof 42);console.log('(Which is also the meaning of life.)');
+console.log(typeof true);console.log('Is it Trully true, or is it a false true? (It is a true boolean)');
