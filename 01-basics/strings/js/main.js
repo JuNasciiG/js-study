686ba782ead30afb74a61479dadb34dc18d67af2
JuNascii <65746158+JuNasciiG@users.noreply.github.com>
2026-02-07 12:17:45 -0300

    Update main.js

--- 01-basics/strings/js/main.js
+++ 01-basics/strings/js/main.js
@@ -1,4 +1,24 @@
-console.log('hello world');
-console.log('JuNascii is a' );console.log(typeof'JuNascii');
-console.log('42 is a');console.log(typeof 42);console.log('(Which is also the meaning of life.)');
-console.log(typeof true);console.log('Is it Trully true, or is it a false true? (It is a true boolean)');
+console.log("salve raizen seu bobo")
+//console.log serve para mandar mensagens no console de desenvolvedor do browser
+
+//typeof retorna o tipo do operando
+
+console.log(typeof 'raizen')
+//typeof retorna STRING
+
+console.log(typeof 53)
+//typeof retorna NUMBER
+
+console.log(typeof true)
+//typeof retorna BOOLEAN
+
+console.log(typeof "Olá")         // "string"
+console.log(typeof 42)            // "number"
+console.log(typeof true)          // "boolean"
+console.log(typeof undefined)     // "undefined"
+console.log(typeof 10n)           // "bigint"
+console.log(typeof Symbol())      // "symbol"
+console.log(typeof function(){})  // "function"
+console.log(typeof {a: 1})        // "object"
+console.log(typeof [1, 2])        // "object" (array é um objeto)
+console.log(typeof null)          // "object" (bug histórico famoso)
